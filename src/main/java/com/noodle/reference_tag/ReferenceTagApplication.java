7e25e7aece199d40dbb3495d7370fbe13e94f9ed
Donald Yubeaton <nasusneverna@gmail.com>
2024-06-03 18:40:37 -0400

    Project Start Up

---
+++ src/main/java/com/noodle/reference_tag/ReferenceTagApplication.java
@@ -0,0 +1,13 @@
+package com.noodle.reference_tag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceTagApplication.class, args);
+	}
+
+}
