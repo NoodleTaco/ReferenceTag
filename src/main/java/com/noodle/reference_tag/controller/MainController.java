da8c968bef2d64208ab14c0087a2e6e63a627897
Donald Yubeaton <nasusneverna@gmail.com>
2024-06-18 15:30:24 -0400

    Tag and Image display work and fixed add image bug

--- src/main/java/com/noodle/reference_tag/controller/MainController.java
+++ src/main/java/com/noodle/reference_tag/controller/MainController.java
@@ -6,7 +6,12 @@ import com.noodle.reference_tag.domain.TagEntity;
 import com.noodle.reference_tag.service.ImageService;
 import com.noodle.reference_tag.service.TagService;
 import com.noodle.reference_tag.util.NotificationUtil;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 import javafx.stage.FileChooser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,8 +19,10 @@ import org.springframework.stereotype.Controller;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -24,7 +31,6 @@ public class MainController {
 
     private final TagService tagService;
     private final StageInitializer stageInitializer;
-
     private final ImageService imageService;
 
     @Autowired
@@ -34,6 +40,19 @@ public class MainController {
         this.imageService = imageService;
     }
 
+    //FXML Properties
+
+    @FXML
+    private ListView<TagEntity> allTagListView;
+
+    @FXML
+    private TilePane imageTilePane;
+
+
+    public void initialize(){
+        refreshAllTagListView();
+        refreshImageTilePane();
+    }
 
     /**
      * Called when the Create Tag Button is activated
@@ -61,15 +80,14 @@ public class MainController {
                     newTag.setName(tagName);
                     tagService.save(newTag);
                     NotificationUtil.showNotification("Tag created successfully.", stageInitializer.getPrimaryStage());
-                    //TODO: Refresh UI After This
+                    refreshAllTagListView();
                 }
 
             }
         });
     }
 
-    public void onAddImageButtonAction(){
-        //TODO: Does not work
+    public void onAddImageButtonAction() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Select Image");
         fileChooser.getExtensionFilters().addAll(
@@ -78,27 +96,66 @@ public class MainController {
 
         File selectedFile = fileChooser.showOpenDialog(stageInitializer.getPrimaryStage());
 
-        if(selectedFile != null){
+        if (selectedFile != null) {
             String imagePath = selectedFile.getAbsolutePath();
             Optional<ImageEntity> existingImage = imageService.findByPath(imagePath);
             if (existingImage.isPresent()) {
                 NotificationUtil.showNotification("Image already exists in the database.", stageInitializer.getPrimaryStage());
-            }
-            else{
+            } else {
                 try {
-                    // Copy the image file to a designated folder within your application
-                    String appImagesFolder = "images";
-                    Files.copy(Paths.get(imagePath), Paths.get(appImagesFolder, selectedFile.getName()), StandardCopyOption.REPLACE_EXISTING);
+                    // Get the path to the "images" directory within the project's resources
+                    Path resourcesPath = Paths.get("src", "main", "resources");
+                    Path imagesDir = resourcesPath.resolve("images");
+
+                    // Create the "images" directory if it doesn't exist
+                    Files.createDirectories(imagesDir);
+
+                    // Copy the image file to the "images" directory
+                    Path destinationPath = imagesDir.resolve(selectedFile.getName());
+                    Files.copy(Paths.get(imagePath), destinationPath, StandardCopyOption.REPLACE_EXISTING);
+
+                    // Store the relative path to the "images" directory
+                    Path relativePath = Paths.get("images", selectedFile.getName());
 
                     ImageEntity newImage = new ImageEntity();
-                    newImage.setPath(Paths.get(appImagesFolder, selectedFile.getName()).toString());
+                    newImage.setPath(relativePath.toString()); // Save the relative path
                     imageService.save(newImage);
                     NotificationUtil.showNotification("Image added successfully.", stageInitializer.getPrimaryStage());
-                    // TODO: Refresh UI After This
+                    refreshImageTilePane();
                 } catch (IOException e) {
-                    System.out.println("Error adding image: " + e.getMessage());
+                    e.printStackTrace(); // Print the stack trace
                 }
             }
         }
     }
+
+
+    /**
+     * Populate the Tag List View using information in the DB
+     */
+    public void refreshAllTagListView(){
+        List<TagEntity> allTags = tagService.findAllTags();
+        allTagListView.getItems().setAll(allTags);
+        imageService.deleteImageById(2L);
+    }
+
+    private void refreshImageTilePane() {
+        imageTilePane.getChildren().clear();
+        List<ImageEntity> images = imageService.findAllImages();
+        for (ImageEntity imageEntity : images) {
+            try {
+                // Load the image as a resource using the stored relative path
+                Image image = new Image(getClass().getResourceAsStream("/" + imageEntity.getPath()));
+                ImageView thumbnailView = new ImageView(image);
+                thumbnailView.setFitHeight(100);
+                thumbnailView.setFitWidth(100);
+                imageTilePane.getChildren().add(thumbnailView);
+            } catch (Exception e) {
+                // Handle any exceptions that occur while loading the image
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
